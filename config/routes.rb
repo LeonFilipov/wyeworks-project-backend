55af5bae1f6a863d518896723fc78a538ea8d154
rodriaguillon <rodrigo.aguillon@fing.edu.uy>
2024-08-31 16:13:25 -0300

    added some users

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :tutors
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]  
 end
