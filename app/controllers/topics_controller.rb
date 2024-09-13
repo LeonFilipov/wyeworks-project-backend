ceffd9c4bbaf4d272f4d509ace3c0d416ecd8ed2
Facundo Larrosa <facundo.l12391@gmail.com>
2024-09-13 20:21:16 -0300

    some adjustments

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,51 +1,61 @@
 class TopicsController < ApplicationController
-    before_action :set_topic, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @topics = Topic.all
-      render json: @topics
-    end
-  
-    def show
-      render json: @topic
-    end
-  
-    def new
-      @topic = Topic.new
-    end
-  
-    def create
-      @topic = Topic.new(topic_params)
-      if @topic.save
-        render json: @topic, status: :created
-      else
-        render json: @topic.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @topic.update(topic_params)
-        render json: @topic, status: :ok
-      else
-        render json: @topic.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @topic.destroy
-      head :no_content
-    end
-  
-    private
-  
-    def set_topic
-      @topic = Topic.find(params[:id])
-    end
-  
-    def topic_params
-      params.require(:topic).permit(:name, :asset)
+  before_action :set_subject
+  before_action :set_topic, only: [:show, :update, :destroy]
+
+  # GET /universities/:university_id/subjects/:subject_id/topics
+  def index
+    @topics = @subject.topics
+    render json: @topics, status: :ok
+  end
+
+  # GET /universities/:university_id/subjects/:subject_id/topics/:id
+  def show
+    render json: @topic, status: :ok
+  end
+
+  # POST /universities/:university_id/subjects/:subject_id/topics
+  def create
+    @topic = @subject.topics.build(topic_params)
+    if @topic.save
+      render json: @topic, status: :created
+    else
+      render json: @topic.errors, status: :unprocessable_entity
     end
   end
+
+  # PATCH/PUT /universities/:university_id/subjects/:subject_id/topics/:id
+  def update
+    if @topic.update(topic_params)
+      render json: @topic, status: :ok
+    else
+      render json: @topic.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /universities/:university_id/subjects/:subject_id/topics/:id
+  def destroy
+    @topic.destroy
+    head :no_content
+  end
+
+  private
+
+  # Set the subject based on the university and subject ID
+  def set_subject
+    @subject = Subject.find(params[:subject_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Subject not found" }, status: :not_found
+  end
+
+  # Set the topic within the context of the subject
+  def set_topic
+    @topic = @subject.topics.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Topic not found" }, status: :not_found
+  end
+
+  # Permit only allowed parameters
+  def topic_params
+    params.require(:topic).permit(:name, :asset)
+  end
+end
--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,59 +1,52 @@
 class UniversitiesController < ApplicationController
-    before_action :set_university, only: [:show, :edit, :update, :destroy]
-  
-    # GET /universities
-    def index
-      @universities = University.all
-    end
-  
-    # GET /universities/:id
-    def show
-    end
-  
-    # GET /universities/new
-    def new
-      @university = University.new
-    end
-  
-    # POST /universities
-    def create
-      @university = University.new(university_params)
-      if @university.save
-        redirect_to @university, notice: 'University was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    # GET /universities/:id/edit
-    def edit
-    end
-  
-    # PATCH/PUT /universities/:id
-    def update
-      if @university.update(university_params)
-        redirect_to @university, notice: 'University was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    # DELETE /universities/:id
-    def destroy
-      @university.destroy
-      redirect_to universities_url, notice: 'University was successfully destroyed.'
-    end
-  
-    private
-  
-    # Método para obtener una universidad por ID
-    def set_university
-      @university = University.find(params[:id])
-    end
-  
-    # Solo permitir parámetros permitidos
-    def university_params
-      params.require(:university).permit(:name, :location)
+  before_action :set_university, only: [:show, :update, :destroy]
+
+  # GET /universities
+  def index
+    @universities = University.all
+    render json: @universities, status: :ok
+  end
+
+  # GET /universities/:id
+  def show
+    render json: @university, status: :ok
+  end
+
+  # POST /universities
+  def create
+    @university = University.new(university_params)
+    if @university.save
+      render json: @university, status: :created
+    else
+      render json: @university.errors, status: :unprocessable_entity
     end
   end
-  
+
+  # PATCH/PUT /universities/:id
+  def update
+    if @university.update(university_params)
+      render json: @university, status: :ok
+    else
+      render json: @university.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /universities/:id
+  def destroy
+    @university.destroy
+    head :no_content
+  end
+
+  private
+
+  # Method to get a university by ID
+  def set_university
+    @university = University.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "University not found" }, status: :not_found
+  end
+
+  def university_params
+    params.require(:university).permit(:name, :location)
+  end
+end
