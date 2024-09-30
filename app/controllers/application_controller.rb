284e0ac019d7eafb5431d633b37841199f8e2e24
rodriaguillon <rodrigo.aguillon@fing.edu.uy>
2024-09-30 20:02:14 -0300

    corrected indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,19 @@ class ApplicationController < ActionController::API
   before_action :authenticate_request
 
   private
-    def authenticate_request
-      if request.headers["Authorization"].present?
-        header = request.headers["Authorization"].split(" ").last
-        begin
-          decoded = JsonWebTokenService.decode(header)
-          @current_user = User.where(id: decoded[:user_id])
-      # @current_user = User.find("e4cf95f4-4f3e-4267-b3f1-78cc0e8a6fa4")
-        rescue JWT::DecodeError
-          render json: { error: "Invalid token" }, status: 401
-        end
-      else
-        render json: { error: "Authorization header is missing" }, status: 400
+
+  def authenticate_request
+    if request.headers["Authorization"].present?
+      header = request.headers["Authorization"].split(" ").last
+      begin
+        decoded = JsonWebTokenService.decode(header)
+        @current_user = User.where(id: decoded[:user_id])
+        # @current_user = User.find("e4cf95f4-4f3e-4267-b3f1-78cc0e8a6fa4")
+      rescue JWT::DecodeError
+        render json: { error: "Invalid token" }, status: 401
       end
+    else
+      render json: { error: "Authorization header is missing" }, status: 400
     end
+  end
 end
