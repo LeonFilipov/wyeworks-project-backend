4450af8c73265aeeba0b98c19e68a276dcf6f3d8
FavioCardoso <favio.cardoso@fing.edu.uy>
2024-10-06 15:42:29 -0300

    Fixed Availability & Tentatives Deleted

--- spec/factories/availability_tutors.rb
+++ spec/factories/availability_tutors.rb
@@ -2,8 +2,6 @@ FactoryBot.define do
   factory :availability_tutor do
     tutor { nil }
     description { "MyString" }
-    date_from { "2024-09-21 21:24:53" }
-    date_to { "2024-09-21 21:24:53" }
     effective_date { "2024-09-21 21:24:53" }
     link { "MyString" }
   end
