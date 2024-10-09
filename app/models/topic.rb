016ccdb13bab9919e664b641a47d39cd4a4bdc80
Leon_ <109763055+LeonFilipov@users.noreply.github.com>
2024-10-08 22:33:48 -0300

    Feature/proposed topics refactorization (#36)
    * Removed /tutor_availability GET and comment unnecesary endpoints
    
    * Refactorized /proposed_topics
    
    * Fixed POST /tutor_availability for create a availability and topic
    
    * Fixed message for created topic
    
    * Updated Swagger
    
    * Updated CHANGELOG.md
    
    * Updated CHANGELOG.md and fixed lint

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,7 @@ class Topic < ApplicationRecord
     has_many :users, through: :student_topics
     has_many :availability_tutors
     has_many :users, through: :availability_tutors
+
+    validates :name, uniqueness: true
+    validates :subject_id, :name, presence: true
 end
