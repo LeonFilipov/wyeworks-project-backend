9aa19d30a032a9dbe7a8c9294a0f42b96c8e5fdb
Leon Filipov <leonfilipov2002@gmail.com>
2024-10-13 12:26:09 -0300

    Updated user model test with RSpec and delete unnecesary tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
+  name = Faker::Name.name
   factory :user do
+    name { name }
+    email { Faker::Internet.email(name: name)}
+    uid { "1234567890" }
+    description { "I am a profesor" }
+    image_url { "https://www.google.com" }
   end
 end
---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "UsersControllers", type: :request do
+  describe "GET /index" do
+    
+  end
+end
