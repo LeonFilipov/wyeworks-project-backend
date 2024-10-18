3b5a4ebcb84c478b7bd85932bf410b87b7d24f87
Leon Filipov <leonfilipov2002@gmail.com>
2024-10-18 00:44:37 -0300

    Fixed lint and JWT_SECRET for testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   name = Faker::Name.name
   factory :user do
     name { name }
-    email { Faker::Internet.email(name: name)}
+    email { Faker::Internet.email(name: name) }
     uid { Faker::Number.number(digits: 10) }
     description { Faker::Religion::Bible.quote }
     image_url { Faker::Internet.url }
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe "UsersControllers", type: :request do
     it "returns the current user with correct attributes" do
       get "/profile", headers: { "Authorization" => token }
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body).first.keys).to eq(["id", "name", "email", "description", "image_url"])
+      expect(JSON.parse(response.body).first.keys).to eq([ "id", "name", "email", "description", "image_url" ])
       expect(JSON.parse(response.body).first["name"]).to eq(user.name)
     end
 
     it "returns an error if the token is invalid" do
       get "/profile", headers: { "Authorization" => "invalid_token" }
       expect(response).to have_http_status(401)
-      expect(JSON.parse(response.body)).to eq({"error" => "Invalid token"})
+      expect(JSON.parse(response.body)).to eq({ "error" => "Invalid token" })
     end
 
     it "returns an error if the token is missing" do
       get "/profile"
       expect(response).to have_http_status(400)
-      expect(JSON.parse(response.body)).to eq({"error" => "Authorization header is missing"})
+      expect(JSON.parse(response.body)).to eq({ "error" => "Authorization header is missing" })
     end
   end
 
@@ -31,10 +31,10 @@ RSpec.describe "UsersControllers", type: :request do
         description: "new description",
         non_permited_attribute: "non-editable",
         email: "non-editable" }
-      }, 
+      },
       headers: { "Authorization" => token }
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to eq({"message" => "User updated"})
+      expect(JSON.parse(response.body)).to eq({ "message" => "User updated" })
     end
   end
 end
