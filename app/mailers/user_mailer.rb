bb905c791b195f3b1e8cdc489108122df2d4c6df
FavioCardoso <favio.cardoso@fing.edu.uy>
2024-10-18 20:41:41 -0300

    fix Rubocop

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,19 @@
 class UserMailer < ApplicationMailer
-    default from: email_address_with_name('studycirclewye@gmail.com', 'StudyCircle')
+    default from: email_address_with_name("studycirclewye@gmail.com", "StudyCircle")
 
     def welcome_email_no_AR(user)
         @current_user = user
         mail(
             to: @current_user.email,
-            subject: 'Bienvenido a StudyCircle'
+            subject: "Bienvenido a StudyCircle"
         )
     end
 
     def welcome_email(user)
         @current_user = user
         mail(
-            to: email_address_with_name(@current_user.first.email , @current_user.first.name),
-            subject: 'Bienvenido a StudyCircle'
+            to: email_address_with_name(@current_user.first.email, @current_user.first.name),
+            subject: "Bienvenido a StudyCircle"
         )
     end
-
 end
--- config/application.rb
+++ config/application.rb
@@ -76,11 +76,11 @@ module WyerworksProjectBackend
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
+      address: "smtp.gmail.com",
       port: 587,
-      user_name: 'studycirclewye@gmail.com',
-      password: 'clmb lkbf qokx qgzc',
-      authentication: 'plain',
+      user_name: "studycirclewye@gmail.com",
+      password: "clmb lkbf qokx qgzc",
+      authentication: "plain",
       enable_starttls_auto: true
     }
 
